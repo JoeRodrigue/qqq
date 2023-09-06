2193d3e36242e9ce3cac884a78697ddd2db2c3f5
Joe Rodrigue <joe.rodrigue@gmail.com>
2023-09-06 15:54:09 -0700

    Changed to semantic versioning

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # qqq
-# Version v2023.09.03-4
+# Version v2023.09.4
 
 This package does amazing things.
 
--- src/imppkg/_version.py
+++ src/imppkg/_version.py
@@ -1,4 +1,4 @@
 #! /usr/bin/python3.11
 # -*- coding: utf-8 -*-
 
-__version__ = "v2023.09.03-4"
+__version__ = "v2023.09.4"
